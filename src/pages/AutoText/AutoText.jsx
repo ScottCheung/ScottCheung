4f6394d1db3862cfc7466a72405f97cf88eb3c55
Xianzhezhang97 <z5443003@ad.unsw.edu.au>
2024-05-29 22:42:55 +1000

    deploy

--- src/pages/AutoText/AutoText.jsx
+++ src/pages/AutoText/AutoText.jsx
@@ -1,12 +1,21 @@
 import React, { useState, useEffect, useRef } from "react";
 import RawData from "./RawData";
 import Toast from "./Toast";
+import { motion } from "framer-motion";
 
-const AutoText = () => {
+const RentMsg = () => {
   const theadRef = useRef(null);
   const tbodyRef = useRef(null);
   const [theadWidth, setTheadWidth] = useState("auto");
   const [toast, setToast] = useState(["", 0]);
+
+  // 初始化状态时读取 local storage
+  const [checkedState, setCheckedState] = useState(
+    RawData.map(
+      (item) =>
+        JSON.parse(localStorage.getItem(item["property address"])) || false,
+    ),
+  );
 
   useEffect(() => {
     if (tbodyRef.current) {
@@ -15,18 +24,30 @@ const AutoText = () => {
     }
   }, [RawData]);
 
+  useEffect(() => {
+    // 当 checkedState 更新时，保存到 local storage
+    checkedState.forEach((checked, index) => {
+      localStorage.setItem(
+        RawData[index]["property address"],
+        JSON.stringify(checked),
+      );
+    });
+  }, [checkedState]);
+
   const handleRowClick = (index, item) => {
     const emailContent = generateEmail(item);
-    navigator.clipboard.writeText(emailContent).then(() => {
-      setToast(["复制成功", 700]);
-      setTimeout(() => {
-        setToast(["", 0]);
-      }, 3000);
-    });
+    const updatedCheckedState = checkedState.map((checked, i) =>
+      i === index ? !checked : checked,
+    );
+    setCheckedState(updatedCheckedState);
 
-    const checkbox = document.getElementById(`checkbox-table-search-${index}`);
-    if (checkbox) {
-      checkbox.checked = !checkbox.checked;
+    if (!checkedState[index]) {
+      navigator.clipboard.writeText(emailContent).then(() => {
+        setToast(["Copy Successfully :)", 1000]);
+        setTimeout(() => {
+          setToast(["", 0]);
+        }, 3000);
+      });
     }
   };
 
@@ -34,19 +55,20 @@ const AutoText = () => {
     return `
 Hello ${item.Agent},
 
-My name is ${item.Sender} and I have just graduated from the University of New South Wales. 
-My friend and I are interested in renting the property at ${item["property address"]} 
-and would like to enquire about some specifics to see if the property meets my needs.
-    
-  - What is the earliest time I can view the property?
-  - I would like to enter into a one year contract. 
-    We would like to know the maximum length of the contract.
-  - I see that the rent is ${item.Rent.trim()} per week. 
-    Are there any additional costs that are not included in this amount? Also, 
-    if we sign a long term lease, will we get a discount?
-    
-Thank you for your time and help. I am very interested in this property and look forward to 
-hearing from you so we can proceed further.
+My name is ${item.Sender}, and my friend and I have just graduated from UNSW. 
+We are very interested in renting the property at 📍${item["property address"]}.
+
+We have a few questions to ensure the property meets our needs:
+
+   - What is the earliest time we can inspect the property and move in? 🕰️
+   - Could you please let us know the maximum length of the contract available? 📜
+   - I see that the rent is ${item.Rent} per week. Are there any additional 
+     costs not included in this amount? Also, if we sign a long-term lease, would 
+     there be any possibility of a discount? 💰
+   - Additionally, is there a video available for viewing the property? 📹
+   
+Thank you very much for your time and assistance.😊 We are very interested in this 
+property and look forward to hearing from you soon so we can proceed further.🌟
 
 Kind regards,
 ${item.Sender}
@@ -92,22 +114,23 @@ ${item.Sender}
           {RawData.map((item, index) => (
             <tr
               key={index}
-              onClick={() => handleRowClick(index, item)}
-              className=" border-b text-[20px] dark:bg-gray-800 dark:border-gray-700 hover:bg-sky-50 dark:hover:bg-gray-600 w-[1%]"
+              className="border-b text-[20px] dark:bg-gray-800 dark:border-gray-700 hover:bg-sky-50 dark:hover:bg-gray-600 w-[1%]"
             >
               <td className="p-4">
                 <div className="flex items-center">
                   <input
                     id={`checkbox-table-search-${index}`}
+                    checked={checkedState[index]}
+                    onChange={() => handleRowClick(index, item)}
                     type="checkbox"
-                    className="w-8 h-8 text-blue-600 bg-gray-100 border-gray-300 rounded-full dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                    className="w-8 h-8 text-green-600 bg-green-100 border-gray-300 rounded-full dark:focus:ring-green-600 dark:ring-offset-green-800 dark:focus:ring-offset-green-800 focus:ring-2 dark:bg-green-700 dark:border-green-600"
                   />
                 </div>
               </td>
               <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white text-center w-[7%]">
                 {index + 1}
               </td>
-              <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white w-[7%]">
+              <td className="gap-y-[30px] px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white w-[7%]">
                 <a
                   href={item["Oringinal Link"]}
                   target="_blank"
@@ -117,8 +140,19 @@ ${item.Sender}
                   {item["property address"]}
                 </a>
               </td>
-              <td className="px-6 py-4 text-left text-[15px] w-[70%]">
+              <td className="px-6 py-4 text-left text-base w-[70%]">
                 <pre>{generateEmail(item)}</pre>
+
+                <motion.button
+                  layout
+                  type="button"
+                  onClick={() => handleRowClick(index, item)}
+                  className={`flex justify-center items-center text-white bg-gradient-to-r ${checkedState[index] ? "bg-gray-400 w-auto hover:from-sky-500 hover:via-sky-600 hover:to-sky-700" : "w-full from-sky-500 via-sky-600 to-sky-700"} hover:bg-gradient-to-br dark:focus:ring-sky-800 font-medium rounded-lg text-2xl px-5 py-2.5 text-center me-2 mb-2`}
+                >
+                  {checkedState[index]
+                    ? "Mark as UNFINISH"
+                    : "COPY Message and Mark as FINISH"}
+                </motion.button>
               </td>
             </tr>
           ))}
@@ -129,4 +163,4 @@ ${item.Sender}
   );
 };
 
-export default AutoText;
+export default RentMsg;
