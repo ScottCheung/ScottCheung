5be90c2563c61e63cd96a510f775d7c735aeb667
Xianzhezhang97 <scott5443003@gmail.com>
2024-07-12 10:33:30 +1000

    Fix some bugs | Xianzhe | Fri Jul 12 10:33:30 AEST 2024 | Mac

--- update-timestamp.js
+++ update-timestamp.js
@@ -6,12 +6,32 @@ const currentTime = new Date().toISOString();
 
 // 读取 package.json 文件
 const packageJsonPath = path.resolve(__dirname, "package.json");
+const Log = path.resolve(__dirname, "update-log.json");
 const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
 
 // 更新发布时间戳
 packageJson.lastPublishedAt = currentTime;
 
+// 更新发布时间戳
+packageJson.lastPublishedAt = currentTime;
+
+// 添加地址信息
+packageJson.address = {
+  street: "Wolli Creek",
+  city: "Sydney",
+  state: "NSW",
+  zipcode: "2205",
+};
+
+packageJson.updateInfo = {
+  new: "updateInfo",
+  fix: "updateInfo",
+  feature: "updateInfo",
+  description: "This function adds new information to the package.json file",
+  version: "1.0.0",
+  date: currentTime,
+};
+
 // 将更新后的内容写回 package.json 文件
 fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
-
-console.log(`Updated package.json with current time: ${currentTime}`);
+fs.writeFileSync(Log, JSON.stringify(Log, null, 2);
