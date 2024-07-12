c5ea651ecc3b3c04c9a92455b009a6d545432479
Xianzhezhang97 <scott5443003@gmail.com>
2024-07-12 10:53:06 +1000

    Fix some bugs | Xianzhe | Fri Jul 12 10:53:05 AEST 2024 | Mac

--- update-timestamp.js
+++ update-timestamp.js
@@ -6,32 +6,35 @@ const currentTime = new Date().toISOString();
 
 // 读取 package.json 文件
 const packageJsonPath = path.resolve(__dirname, "package.json");
-const Log = path.resolve(__dirname, "update-log.json");
+const logPath = path.resolve(__dirname, "update-log.json");
 const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
+const log = JSON.parse(fs.readFileSync(logPath, "utf8"));
 
 // 更新发布时间戳
 packageJson.lastPublishedAt = currentTime;
 
-// 更新发布时间戳
-packageJson.lastPublishedAt = currentTime;
-
-// 添加地址信息
-packageJson.address = {
-  street: "Wolli Creek",
-  city: "Sydney",
-  state: "NSW",
-  zipcode: "2205",
-};
-
-packageJson.updateInfo = {
+// 创建新的日志条目
+const newLogEntry = {
   new: "updateInfo",
   fix: "updateInfo",
   feature: "updateInfo",
   description: "This function adds new information to the package.json file",
-  version: "1.0.0",
-  date: currentTime,
+  version: packageJson.version,
+  address: {
+    street: "Wolli Creek",
+    city: "Sydney",
+    state: "NSW",
+    zipcode: "2205",
+  },
 };
 
-// 将更新后的内容写回 package.json 文件
+// 将新日志条目添加到日志字典中，以时间戳作为键
+log[currentTime] = newLogEntry;
+
+// 将更新后的内容写回 package.json 文件和 update-log.json 文件
 fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
-fs.writeFileSync(Log, JSON.stringify(Log, null, 2));
+fs.writeFileSync(logPath, JSON.stringify(log, null, 2));
+
+console.log(
+  `Updated package.json and update-log.json with current time: ${currentTime}`,
+);
