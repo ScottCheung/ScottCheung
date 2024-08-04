84cdfee617c0999e85cd08789a6a6a94553182df
Xianzhezhang97 <scott5443003@gmail.com>
2024-08-05 06:44:20 +1000

    deploy

--- src/conponent/NavBar/Navbar.jsx
+++ src/conponent/NavBar/Navbar.jsx
@@ -646,48 +646,41 @@ function Navbar({ topTextColor, BG, ExpandElement, onHeightChange }) {
       </motion.div>
 
       {/* toTop buttom */}
-      {!isTop && Components.NavBar === 'visible' && (
-        <motion.button
-          key='toTop'
-          initial={{ opacity: 0, scale: 0 }}
-          animate={{
-            opacity: 1,
-            scale: 1,
-            transition: { duration: 0.3, type: 'easeInOut' },
-          }}
-          exit={{ opacity: 0, scale: 0 }}
-          transition={{ duration: 1.2 }}
-          whileTap={{ scale: 0.9, opacity: 1 }}
-          whileHover={{ scale: 1.1, opacity: 1 }}
-          onClick={() => {
-            window.scrollTo({
-              top: 0,
-              behavior: 'smooth',
-            });
-          }}
-          className={`fixed bottom-10 lg:right-20 right-10 z-50  `}
-        >
-          <div className='px-4 py-2 text-white rounded-full lg:scale-150'>
-            <div className='modal-trigger-visblecontainer '>
-              <motion.span
-                initial={{ rotate: 90 }}
-                whileTap={{ scale: 0.9, opacity: 0.8, rotate: 270 }}
-                className='bg-white opacity-70 hover:opacity-100 '
-                style={buttonStyles}
+
+      <motion.button
+        key='toTop'
+        transition={{ duration: 1.2 }}
+        whileTap={{ scale: 0.9, opacity: 1 }}
+        whileHover={{ scale: 1.1, opacity: 1 }}
+        onClick={() => {
+          window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+          });
+        }}
+        className={`fixed bottom-10 transition-all duration-1000 ${isTop || isScrolling || Components.NavBar !== 'visible' ? '-right-[80px]' : 'right-10 lg:right-12'} z-50  `}
+      >
+        <div className='px-4 py-2 text-white rounded-full lg:scale-150'>
+          <div className='modal-trigger-visblecontainer '>
+            <motion.span
+              initial={{ rotate: 90 }}
+              whileTap={{ scale: 0.9, opacity: 0.8, rotate: 270 }}
+              className='bg-white opacity-70 hover:opacity-100 '
+              style={buttonStyles}
+            >
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                viewBox='8 8 20 20'
+                style={iconStyles}
+                className=' w-[18px] h-[18px] text-white rotate-180'
               >
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  viewBox='8 8 20 20'
-                  style={iconStyles}
-                  className=' w-[18px] h-[18px] text-white rotate-180'
-                >
-                  <path d='M23.5587,16.916 C24.1447,17.4999987 24.1467,18.446 23.5647,19.034 L16.6077,26.056 C16.3147,26.352 15.9287,26.4999987 15.5427,26.4999987 C15.1607,26.4999987 14.7787,26.355 14.4867,26.065 C13.8977,25.482 13.8947,24.533 14.4777,23.944 L20.3818,17.984 L14.4408,12.062 C13.8548,11.478 13.8528,10.5279 14.4378,9.941 C15.0218,9.354 15.9738,9.353 16.5588,9.938 L23.5588,16.916 L23.5587,16.916 Z'></path>
-                </svg>
-              </motion.span>
-            </div>
+                <path d='M23.5587,16.916 C24.1447,17.4999987 24.1467,18.446 23.5647,19.034 L16.6077,26.056 C16.3147,26.352 15.9287,26.4999987 15.5427,26.4999987 C15.1607,26.4999987 14.7787,26.355 14.4867,26.065 C13.8977,25.482 13.8947,24.533 14.4777,23.944 L20.3818,17.984 L14.4408,12.062 C13.8548,11.478 13.8528,10.5279 14.4378,9.941 C15.0218,9.354 15.9738,9.353 16.5588,9.938 L23.5588,16.916 L23.5587,16.916 Z'></path>
+              </svg>
+            </motion.span>
           </div>
-        </motion.button>
-      )}
+        </div>
+      </motion.button>
+
       <div className='relative z-50 '>
         <div
           id='toast-root'
