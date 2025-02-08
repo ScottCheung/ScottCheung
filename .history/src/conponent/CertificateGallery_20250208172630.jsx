bd456d025bfa2202f27ea1e14a3b643a6d6b10ac
Xianzhezhang97 <scott5443003@gmail.com>
2025-02-08 17:32:22 +0800

    Fix Some Bugs🐞 | Xianzhe | Sat Feb  8 17:32:21 CST 2025 | xianzhes-MacBook-Pro-9.local

--- .history/src/conponent/CertificateGallery_20250208172630.jsx
+++ .history/src/conponent/CertificateGallery_20250208172630.jsx
@@ -3,7 +3,6 @@
 import React, { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 import Database from '../data/Database.json';
-
 import { useLanguage } from '../help/helpFunction';
 
 function CertificateGallery() {
