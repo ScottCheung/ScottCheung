27bda7e861ef957380e303dda4a52d6d0164add6
Xianzhezhang97 <scott5443003@gmail.com>
2025-03-22 19:04:01 +1100

    deploy

--- src/pages/Resume/Resumecheck.jsx
+++ src/pages/Resume/Resumecheck.jsx
@@ -54,7 +54,7 @@ export default function Errorpage() {
   const h2 =
     'flex animate__animated transition-all duration-500  animate__fadeInUp mt-4 py-[10px] font-bold tracking-tight text-gray-700 text-[15px] md:text-[20px] ';
   return (
-    <div className='min-h-screen overflow-hidden bg-white'>
+    <div className='flex items-center justify-center h-screen overflow-hidden bg-white'>
       <Navbar />
       <link
         rel='stylesheet'
@@ -66,124 +66,111 @@ export default function Errorpage() {
         href='.../style/uicons/css/all/all.css'
         type='text/css'
       />
-      <main className='flex justify-center h-full px-6 py-24 transition-all duration-500 animate__animated animate__fadeIn place-items-center sm:py-32 lg:px-8 '>
-        <div className='text-center '>
-          <motion.i
-            initial={{ scale: 0, y: '-90px', opacity: 0 }}
-            animate={{ scale: 1, y: '0px', opacity: 1 }}
-            transition={{ duration: 0.7 }}
-            className={`animate__animated animate__fadeInUp text-[70px] md:text-[150px] font-semibold fi ${isLatestVersion ? ' fi-rr-check-circle text-green-700 ' : 'text-yellow-700 fi-rs-exclamation'}`}
-          ></motion.i>
-          <i class=''></i>
-          <motion.p
-            initial={{ scale: 0, y: '-90px', opacity: 0 }}
-            animate={{ scale: 1, y: '0px', opacity: 1 }}
-            transition={{ duration: 0.7 }}
-            className='animate__animated animate__fadeInUp text-[30px] md:text-[50px] font-semibold text-gray-700'
-          >
-            {Message.FirstHeader[lang]} {version}
-          </motion.p>
-          <div className='flex items-center justify-center mt-10 place-items-center gap-x-24'>
-            {isLatestVersion !== null && (
-              <motion.div
-                initial={{ scale: 0.5, opacity: 0 }}
-                animate={{ scale: 1, opacity: 1 }}
-                transition={{ duration: 0.7 }}
-                className='transition-all duration-500 animate__animated animate__zoomIn '
-              >
-                {isLatestVersion ?
-                  <motion.div className='flex gap-x-[30px] items-center'>
-                    <i class='flex text-5xl text-green-500 fi fi-br-check'></i>
-                    <motion.h1
-                      initial={{ scale: 0.9, y: '60px', opacity: 0 }}
-                      animate={{ scale: 1, y: '0px', opacity: 1 }}
-                      transition={{ duration: 0.8 }}
-                      style={{ animationDelay: `0.2s` }}
-                      className={h2}
-                    >
-                      {Message.LatestVersion[lang]}
-                    </motion.h1>
-                  </motion.div>
-                : <motion.div className='flex gap-x-[30px] items-center'>
-                    <i class='flex text-5xl text-yellow-500 fi fi-br-cross'></i>
-                    <motion.h1
-                      initial={{ scale: 0.9, y: '60px', opacity: 0 }}
-                      animate={{ scale: 1, y: '0px', opacity: 1 }}
-                      transition={{ duration: 0.8 }}
-                      style={{ animationDelay: `0.2s` }}
-                      className={h2}
-                    >
-                      {Message.OldVersion[lang]}
-                    </motion.h1>
-                  </motion.div>
-                }
-              </motion.div>
-            )}
-          </div>
-          {isLatestVersion === true && (
-            <div className='flex gap-[15px] justify-center py-[40px] flex-wrap'>
-              <motion.a
-                style={{ animationDelay: '0.2s' }}
-                href='/'
-                className={button}
-              >
-                <i class='flex fi fi-rr-home text-[20px]'></i>
-                {['Home', '主页'][lang]}
-              </motion.a>
-              <motion.a
-                style={{ animationDelay: '0.2s' }}
-                href='/award'
-                className={button}
-              >
-                <i class='flex fi fi-rr-diploma text-[20px]'></i>
-                {['Awards', '荣誉'][lang]}
-              </motion.a>
-              <motion.a
-                style={{ animationDelay: '0.2s' }}
-                href='/scholarship'
-                className={button}
-              >
-                <i class='flex fi fi-rr-trophy-star text-[20px]'></i>
-                {['Scholarships', '奖学金'][lang]}
-              </motion.a>
-            </div>
-          )}
-          {isLatestVersion === false && (
-            <div className='flex gap-[30px] justify-center py-[40px]'>
-              <motion.a
-                style={{ animationDelay: '0.2s' }}
-                href={Message.btn1web}
-                className={button}
-              >
-                <i class='flex fi fi-sr-galaxy-star text-[20px]'></i>
 
-                {Message.btn1[lang]}
-              </motion.a>
-              <motion.a
-                style={{ animationDelay: '0.4s' }}
-                href={`https://github.com/Xianzhezhang97/download/raw/main/CV%20%7C%20Scott%20Cheung.pdf`}
-                className={button}
-              >
-                <i class='flex fi fi-rr-file-download text-[20px]'></i>
-                {Message.btn2[lang]}
-              </motion.a>
-            </div>
+      <div
+        className={`${isLatestVersion ? '  from-green-500/20 ' : 'from-yellow-500/20 '} bg-gradient-to-b from-0% via-white via-[100px] to-gray-100 flex flex-col items-center justify-center w-full h-full text-center `}
+      >
+        <motion.i
+          initial={{ scale: 0, y: '-90px', opacity: 0 }}
+          animate={{ scale: 1, y: '0px', opacity: 1 }}
+          transition={{ duration: 0.7 }}
+          className={`animate__animated animate__fadeInUp text-[70px] md:text-[150px] font-semibold fi ${isLatestVersion ? ' fi-rr-check-circle text-green-700 ' : 'text-yellow-700 fi-rs-exclamation'}`}
+        ></motion.i>
+        <motion.p
+          initial={{ scale: 0, y: '-90px', opacity: 0 }}
+          animate={{ scale: 1, y: '0px', opacity: 1 }}
+          transition={{ duration: 0.7 }}
+          className='animate__animated animate__fadeInUp text-[30px] md:text-[50px] font-semibold text-gray-700'
+        >
+          {Message.FirstHeader[lang]} {version}
+        </motion.p>
+        <div className='flex items-center justify-center mt-10 place-items-center gap-x-24'>
+          {isLatestVersion !== null && (
+            <motion.div
+              initial={{ scale: 0.5, opacity: 0 }}
+              animate={{ scale: 1, opacity: 1 }}
+              transition={{ duration: 0.7 }}
+              className='transition-all duration-500 animate__animated animate__zoomIn '
+            >
+              {isLatestVersion ?
+                <motion.div className='flex gap-x-[30px] items-center'>
+                  <i class='flex text-5xl text-green-500 fi fi-br-check'></i>
+                  <motion.h1
+                    initial={{ scale: 0.9, y: '60px', opacity: 0 }}
+                    animate={{ scale: 1, y: '0px', opacity: 1 }}
+                    transition={{ duration: 0.8 }}
+                    style={{ animationDelay: `0.2s` }}
+                    className={h2}
+                  >
+                    {Message.LatestVersion[lang]}
+                  </motion.h1>
+                </motion.div>
+              : <motion.div className='flex gap-x-[30px] items-center'>
+                  <i class='flex text-5xl text-yellow-500 fi fi-br-cross'></i>
+                  <motion.h1
+                    initial={{ scale: 0.9, y: '60px', opacity: 0 }}
+                    animate={{ scale: 1, y: '0px', opacity: 1 }}
+                    transition={{ duration: 0.8 }}
+                    style={{ animationDelay: `0.2s` }}
+                    className={h2}
+                  >
+                    {Message.OldVersion[lang]}
+                  </motion.h1>
+                </motion.div>
+              }
+            </motion.div>
           )}
         </div>
-      </main>
-      <div
-        style={{
-          backgroundImage: `url(${Message.bg})`,
-          position: 'absolute',
-          top: 0,
-          bottom: 0,
-          left: 0,
-          right: 0,
-          zIndex: -1,
-          backgroundPosition: 'center center',
-          backgroundSize: 'cover',
-        }}
-      ></div>
+        {isLatestVersion === true && (
+          <div className='flex gap-[15px] justify-center py-[40px] flex-wrap'>
+            <motion.a
+              style={{ animationDelay: '0.2s' }}
+              href='/'
+              className={button}
+            >
+              <i class='flex fi fi-rr-home text-[20px]'></i>
+              {['Home', '主页'][lang]}
+            </motion.a>
+            <motion.a
+              style={{ animationDelay: '0.2s' }}
+              href='/award'
+              className={button}
+            >
+              <i class='flex fi fi-rr-diploma text-[20px]'></i>
+              {['Awards', '荣誉'][lang]}
+            </motion.a>
+            <motion.a
+              style={{ animationDelay: '0.2s' }}
+              href='/scholarship'
+              className={button}
+            >
+              <i class='flex fi fi-rr-trophy-star text-[20px]'></i>
+              {['Scholarships', '奖学金'][lang]}
+            </motion.a>
+          </div>
+        )}
+        {isLatestVersion === false && (
+          <div className='flex gap-[30px] justify-center py-[40px]'>
+            <motion.a
+              style={{ animationDelay: '0.2s' }}
+              href={Message.btn1web}
+              className={button}
+            >
+              <i class='flex fi fi-sr-galaxy-star text-[20px]'></i>
+
+              {Message.btn1[lang]}
+            </motion.a>
+            <motion.a
+              style={{ animationDelay: '0.4s' }}
+              href={`https://github.com/Xianzhezhang97/download/raw/main/CV%20%7C%20Scott%20Cheung.pdf`}
+              className={button}
+            >
+              <i class='flex fi fi-rr-file-download text-[20px]'></i>
+              {Message.btn2[lang]}
+            </motion.a>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
