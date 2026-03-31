6724e37e806021374ca56ea6232b3675947a4e1b
Xianzhezhang97 <scott5443003@gmail.com>
2026-04-01 02:57:29 +1000

    Fix some bugs | Scott Cheung | Wed Apr  1 00:32:25 AEST 2026

--- src/conponent/NavBar/Toggle.tsx
+++ src/conponent/NavBar/Toggle.tsx
@@ -1,12 +1,14 @@
+/** @format */
+
 import React from 'react';
 import { motion } from 'framer-motion';
 
-interface SvgSwitcherProps {
+interface ToggleProps {
   isExpanded: boolean;
   style?: string;
 }
 
-const SvgSwitcher: React.FC<SvgSwitcherProps> = ({ isExpanded, style }) => {
+const Toggle: React.FC<ToggleProps> = ({ isExpanded, style }) => {
   const defaultStyle = 'w-12 h-12  stroke-[2px]';
   const svgStyle = style || defaultStyle;
 
@@ -29,13 +31,15 @@ const SvgSwitcher: React.FC<SvgSwitcherProps> = ({ isExpanded, style }) => {
           strokeLinejoin='round'
           fillRule='evenodd'
           animate={{
-            points: isExpanded
-              ? '15 7.72 8.5 1.13 2 7.72'
+            points:
+              isExpanded ?
+                '15 7.72 8.5 1.13 2 7.72'
               : '15 1.13 8.5 7.72 2 1.13',
           }}
           initial={{
-            points: isExpanded
-              ? '15 1.13 8.5 7.72 2 1.13'
+            points:
+              isExpanded ?
+                '15 1.13 8.5 7.72 2 1.13'
               : '15 7.72 8.5 1.13 2 7.72',
           }}
           transition={{ duration: 0.5 }}
@@ -45,4 +49,4 @@ const SvgSwitcher: React.FC<SvgSwitcherProps> = ({ isExpanded, style }) => {
   );
 };
 
-export default SvgSwitcher;
+export default Toggle;
